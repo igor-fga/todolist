46c42f762853e207db165274069fe683a15d34a5
igor-fga <igor.andrade@unico.io>
2021-09-09 19:38:25 -0300

    Concluindo desafio

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -2,22 +2,46 @@ import React, { useState } from 'react';
 import { StyleSheet, View } from 'react-native';
 
 import { Header } from '../components/Header';
+import { ItemWrapper } from '../components/ItemWrapper';
 import { Task, TasksList } from '../components/TasksList';
 import { TodoInput } from '../components/TodoInput';
+
+
 
 export function Home() {
   const [tasks, setTasks] = useState<Task[]>([]);
 
   function handleAddTask(newTaskTitle: string) {
-    //TODO - add new task
+    const data = {
+      id: new Date().getTime(),
+      title: newTaskTitle,
+      done: false
+    }
+
+    setTasks(oldTasks => [...oldTasks, data])
   }
 
   function handleToggleTaskDone(id: number) {
-    //TODO - toggle task done if exists
+
+    const updatedTasks = tasks.map(task => ({ ...task }))
+
+    const foundItem = updatedTasks.find(item => item.id === id);
+
+    if (!foundItem)
+      return;
+    
+    foundItem.done = !foundItem.done;
+    setTasks(updatedTasks)
+
+    //let index = updatedTasks.findIndex(el => el.id === id);
+    //updatedTasks[index] = { ...updatedTasks[index], done: !updatedTasks[index].done }
+    setTasks(updatedTasks)
   }
 
   function handleRemoveTask(id: number) {
-    //TODO - remove task from state
+    setTasks(oldTasks => oldTasks.filter(
+      task => task.id !== id
+    ))
   }
 
   return (
@@ -26,10 +50,10 @@ export function Home() {
 
       <TodoInput addTask={handleAddTask} />
 
-      <TasksList 
-        tasks={tasks} 
+      <TasksList
+        tasks={tasks}
         toggleTaskDone={handleToggleTaskDone}
-        removeTask={handleRemoveTask} 
+        removeTask={handleRemoveTask}
       />
     </View>
   )
